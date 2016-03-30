90d2c186bdce6f7aa54b4ecb715e4f2c4c90c7d9
jean-philippe serafin <serafinjp@gmail.com>
2016-03-30 11:42:36 +0000

    always pickup database connection from django internals

--- dj_pq/__init__.py
+++ dj_pq/__init__.py
@@ -1,9 +1,18 @@
 from django.db import connections
 
-from pq.tasks import PQ, Queue
+from pq.tasks import PQ, Queue as BaseQueue
 
 
 _pqs = dict()
+
+
+class Queue(BaseQueue):
+    def __init__(self, name, connection_name):
+        super(Queue, self).__init__(name)
+        self.connection_name = connection_name
+
+    def _conn(self):
+        return connections[self.connection_name].cursor().connection
 
 
 def get_queue(name, connection_name='default'):
@@ -12,6 +21,6 @@ def get_queue(name, connection_name='default'):
 
     except KeyError:
         return _pqs.setdefault(connection_name, PQ(
-            connections[connection_name].cursor().connection,
+            connection_name,
             queue_class=Queue,
         ))[name]
